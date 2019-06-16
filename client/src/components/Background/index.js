159e847b4dcf161a7c0e0f95a0bf4a590376cae0
rodrigodmoreira <mdmartinsmdm@gmail.com>
2019-06-16 00:16:24 -0300

    Reformulado componente background

--- client/src/components/Background/index.js
+++ client/src/components/Background/index.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import BackgroundImage from '../../@robsales/assets/Background.svg'
+
 const Background = props => (
   <Wrapper>
     <Children>{props.children}</Children>
-    <Ellipsis zIndex='2' backgroundColor='white' size={1000} />
-    <Ellipsis zIndex='1' backgroundColor='#FDE3A8' size={1500} />
   </Wrapper>
 )
 
@@ -19,34 +19,14 @@ const Children = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-
-  z-index: 3;
 `
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 
-  z-index: 0;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-
+  background-image: url(${BackgroundImage});
+  background-repeat: no-repeat;
+  background-size: cover;
   background-color: #764C24;
-`
-
-const Ellipsis = styled.div`
-  position: fixed;
-  top: -3vh;
-  right: -50vw;
-
-  z-index: ${props => props.zIndex ? props.zIndex : 0};
-
-  width: ${props => props.size}px;
-  height: ${props => props.size}px;
-  border-radius: ${props => props.size/2}px;
-
-  background-color: ${props => props.backgroundColor};
 `
